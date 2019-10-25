b02e8496bda10be90abf287c87cfdd4823d781ca
Yasser Souri <yassersouri@gmail.com>
2019-10-25 17:13:42 +0200

    use os.path.expanduser

--- utils/settings.py
+++ utils/settings.py
@@ -1,14 +1,17 @@
+import os
+
 from utils import AttrDict
 
 config = AttrDict()
+user_home_dir = os.path.expanduser("~")
 
 config.video_list = "./misc/vid_list.txt"
-config.video_root = "/home/tridiv/epic_kitchens/EPIC_KITCHENS_2018/videos"
+config.video_root = os.path.join(user_home_dir, "epic_kitchens/EPIC_KITCHENS_2018/videos")
 config.weights = "./weights/ek18-2gpu-e2e-faster-rcnn-R-101-FPN_1x.pkl"
 config.cfg_file = "./config/ek18-2gpu-e2e-faster-rcnn-R-101-FPN_1x.yaml"
 config.sample_fps = 60
 config.top_predictions = 100
-config.out_path = "/home/tridiv/detections"
+config.out_path = os.path.join(user_home_dir, "detections")
 
 try:
     from utils.local_settings import *
